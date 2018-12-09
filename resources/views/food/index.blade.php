3bf9a17ac28ed096ef86956ff78fefda1565df25
TruliParadajz <nargz44@gmail.com>
2018-12-09 23:11:37 +0100

    First migration

---
+++ resources/views/food/index.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+    <header>
+        <h1>Header</h1>
+    </header>
+    <body>
+            <div>
+                <p>Fooooood!!!</p>
+            </div>
+    </body>
+    <footer>
+        <h3>Footer</h3>
+    </footer>
+</html>
