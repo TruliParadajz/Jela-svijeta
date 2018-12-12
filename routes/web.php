85b193f512697f96fc16aa530bfe320a50564660
TruliParadajz <nargz44@gmail.com>
2018-12-12 16:12:02 +0100

    Views Added

--- routes/web.php
+++ routes/web.php
@@ -10,28 +10,7 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-/*Route::get('/', function () {
-    return view('food.index');
-});
-
-Route::get('/{id}/{name}', function($id, $name) {
-    return "Num = ".$id." ".$name;
-});
-
-Route::get('admin/posts/example', array('as' => 'admin.home', function(){
-    $url = route('admin.home');
-    return "URL is: ". $url;
-}));
-*/
-//Route::get('/meal/{id}', 'MealsController@index');
-
-//Route::resource('meals', 'MealsController');
-
-//Route::get('/meals', 'MealsController@meal');
-
 Route::get('/page/{per_page}/{page}', 'ShowTargetedController@getThose');
 Route::get('/', 'ShowAllController@showAll');
 Route::get('/category/{categoryNum}', 'ShowPerCategory@getCategory');
 Route::get('/tag/{tagNum}', 'ShowPerTagController@getTag');
-//&{category}&{tags}&{with}&{diff_time?}
